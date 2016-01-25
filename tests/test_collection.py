786e3405b1765d59a7d5bc39755d53b1e71f1ac1
Petter Reinholdtsen <pere@hungry.com>
2016-01-25 22:36:14 +0000

    Test vcalendar item with slash in the UID field.
    Detect if such item is properly handled by resource_from_path().
    
    Signed-off-by: Petter Reinholdtsen <pere@hungry.com>

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -9,10 +9,12 @@ import unittest
 
 import calypso.config
 from calypso.webdav import Collection
+from calypso import paths
 
 
 class TestCollection(unittest.TestCase):
     test_vcard = "tests/data/import.vcard"
+    test_resource_with_slash = "tests/data/from-tripsync.ics"
 
     def setUp(self):
         self.tmpdir = tempfile.mkdtemp()
@@ -29,3 +31,13 @@ class TestCollection(unittest.TestCase):
         self.assertEqual(len(collection.items), 2)
         org = u'Universitetet i Tromsø'
         self.assertTrue(org == collection.items[0].object.org.value[0])
+
+    def test_uid_with_slash(self):
+        collection = Collection("/")
+        self.assertTrue(collection.import_file(self.test_resource_with_slash))
+        self.assertEqual(len(collection.items), 1)
+        veventuid = collection.items[0].object.vevent.uid.value
+        r = paths.resource_from_path(veventuid)
+        c = paths.collection_from_path(veventuid)
+        self.assertTrue(r == veventuid)
+        self.assertTrue("/" == c)
