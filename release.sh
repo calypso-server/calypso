b3d71430fa1e53e61c2581b3d331a8457010c55a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2016-04-09 11:50:17 +0000

    Remove Keith-specific scripts for releasing and uploading.

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-VERSION=`python ./calypso.py --version`
-
-git-buildpackage --git-debian-branch=debian
-
-git archive --format=tar.gz --prefix=calypso-$VERSION/ -o ../calypso-$VERSION.tar.gz master
--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-VERSION=`python ./calypso.py --version`
-
-(cd ../build-area && dput calypso_$VERSION_*.changes)
-
-scp ../calypso-$VERSION.tar.gz keithp.com:/var/www/calypso
